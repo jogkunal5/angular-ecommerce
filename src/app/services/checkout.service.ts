a8958169950bd41522b3de5178b1966bfbbfd6af
Kunal Jog <kunal.jog@tatacommunications.com>
2022-07-28 10:27:04 +0530

    new changes

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 import { Purchase } from '../common/purchase';
 
 @Injectable({
@@ -11,10 +12,16 @@ export class CheckoutService {
 
   private purchaseUrl = environment.creativecartApiUrl + "/checkout/purchase";
 
+  private paymentIntentUrl = environment.creativecartApiUrl + "/checkout/payment-intent";
+
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase): Observable<any> {
-    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);    
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
   }
-  
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
+  }
+
 }
